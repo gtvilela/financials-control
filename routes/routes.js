d7b1f780a09f097f7343449c1523333677fcd1b4
gtvilela <gabrielathalitasv@gmail.com>
2020-09-21 12:59:39 -0300

    first commit

---
+++ routes/routes.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const transactionRouter = express.Router();
+
+transactionRouter.get('/', async(req, res) => {
+    const {query} = req;
+
+    try {
+        
+    if(!query.period) {
+        throw new Error(`É necessário informar o parâmetro "period", cujo valor deve estar no formato yyyy-mm`)
+    }
+
+    res.send({length: 2, transactions: ['transaction 1', 'transaction 2']})
+    } catch ({message}) {
+        console.log(message);
+        res.status(400).send({error: message})
+    }
+})
+
+module.exports = transactionRouter;
